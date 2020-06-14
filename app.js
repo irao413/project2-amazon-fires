1cf7c2a384959c22a3f4363a25cf528126b89846
Ivy Tiongco <ivydoreen@gmail.com>
2020-06-14 13:21:49 -0700

    update app.py, delete fire.db, and add app.js, index.html, style.css

---
+++ app.js
@@ -0,0 +1,122 @@
+// Step 1: Set up our chart
+//= ================================
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Step 2: Create an SVG wrapper,
+// append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+// =================================
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Step 3:
+// Import data from the JSON response from Flask
+// =================================
+var url ="http://127.0.0.1:5000/get_db_data";
+
+d3.json(url).then(function(cattleData) {
+  // Step 4: Parse the data
+  // Format the data and convert to numerical and year values
+  // =================================
+  // Create a function to parse year and time
+  var parseTime = d3.timeParse("%Y");
+
+  // Format the data
+  cattleData.forEach(function(data) {
+    data.year = parseTime(data.year);
+    data.cattle = +data.cattle;
+    data.cow_milk = +data.cow_milk;
+  });
+
+  // Step 5: Create the scales for the chart
+  // =================================
+  var xTimeScale = d3.scaleTime()
+    .domain(d3.extent(cattleData, d => d.year))
+    .range([0, width]);
+
+  var yLinearScale = d3.scaleLinear().range([height, 0]);
+
+  // Step 6: Set up the y-axis domain
+  // ==============================================
+  // @NEW! determine the max y value
+  // find the max of the cattle data
+  var cattleMax = d3.max(cattleData, d => d.cattle);
+
+  // find the max of the cow_milk data
+  var cow_milkMax = d3.max(cattleData, d => d.cow_milk);
+
+  var yMax;
+  if (cattleMax > cow_milkMax) {
+    yMax = cattleMax;
+  }
+  else {
+    yMax = cow_milkMax;
+  }
+
+  // var yMax = cattleMax > cow_milkMax ? cattleMax : cow_milkMax;
+
+  // Use the yMax value to set the yLinearScale domain
+  yLinearScale.domain([0, yMax]);
+
+
+  // Step 7: Create the axes
+  // =================================
+  var bottomAxis = d3.axisBottom(xTimeScale).tickFormat(d3.timeFormat("%d-%b"));
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // Step 8: Append the axes to the chartGroup
+  // ==============================================
+  // Add x-axis
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  // Add y-axis
+  chartGroup.append("g").call(leftAxis);
+
+  // Step 9: Set up two line generators and append two SVG paths
+  // ==============================================
+
+  // Line generator for cattle data
+  var line1 = d3.line()
+    .x(d => xTimeScale(d.year))
+    .y(d => yLinearScale(d.cattle));
+
+  // Line generator for cow_milk data
+  var line2 = d3.line()
+    .x(d => xTimeScale(d.year))
+    .y(d => yLinearScale(d.cow_milk));
+
+  // Append a path for line1
+  chartGroup
+    .append("path")
+    .attr("d", line1(cattleData))
+    .classed("line green", true);
+
+  // Append a path for line2
+  chartGroup
+    .data([cattleData])
+    .append("path")
+    .attr("d", line2)
+    .classed("line orange", true);
+
+}).catch(function(error) {
+  console.log(error);
+});
--- app.py
+++ app.py
@@ -6,94 +6,41 @@
 # Postgres tables = brazil_fires, brazil_states_deforestation
 # sqlite table = brazil_fires
 # sqlite database = fire.db
+# mongo database = brazil_db
+# mongo collection = cattle_milk
 
-# https://ucsd.bootcampcontent.com/UCSD-Coding-Bootcamp/ucsd-sd-data-pt-01-2020-u-c/blob/master/01-Lesson-Plans/10-Advanced-Data-Storage-and-Retrieval/3/Activities/10-Ins_Flask_with_ORM/Solved/app.py
+# http://127.0.0.1:5000/get_db_data
 
-import numpy as np
+from flask import Flask, render_template, jsonify
+from flask_pymongo import PyMongo
 
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-
-from flask import Flask, jsonify
-
-
-#################################################
-# Database Setup
-#################################################
-engine = create_engine("sqlite:///fire.db")
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-
-# Save reference to the table
-Fire = Base.classes.brazil_fires
-
-#################################################
-# Flask Setup
-#################################################
 app = Flask(__name__)
 
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/brazil_db")
 
-#################################################
-# Flask Routes
-#################################################
-
+# Route to render index.html template using data from Mongo
 @app.route("/")
+
 def welcome():
-    """List all available api routes."""
-    return (
+    return(
+        f"Welcome to my home page!<br/>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
+        f"/get_db_data<br/>"
     )
 
+@app.route('/get_db_data')
+def get_data():
+    # get_data_from_db is a custom function you write to query your database
+    db = mongo.db.cattle_milk
 
-@app.route("/api/v1.0/names")
-def names():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+    db_data = []
+    for item in db.find():
+        item.pop('_id')
+        db_data.append(item)
 
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Fire.state).all()
+    # return jsonify(db_data)
+    return jsonify(db_data)
 
-    session.close()
-
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
-
-    return jsonify(all_names)
-
-
-@app.route("/api/v1.0/passengers")
-def passengers():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Fire.year, Fire.month, Fire.state, Fire.latitude, Fire.longitude, Fire.firespots).all()
-
-    session.close()
-
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_fires = []
-    for year, month, state, latitude, longitude, firespots in results:
-        fire_dict = {}
-        fire_dict["year"] = year
-        fire_dict["month"] = month
-        fire_dict["state"] = state
-        fire_dict["latitude"] = latitude
-        fire_dict["longitude"] = longitude
-        fire_dict["firespots"] = firespots
-        all_fires.append(fire_dict)
-
-    return jsonify(all_fires)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
