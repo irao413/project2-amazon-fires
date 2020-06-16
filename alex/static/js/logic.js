f93d74e5ab623e452598a12dc15f5295c00811c3
alexanderdyan <58492089+alexanderdyan@users.noreply.github.com>
2020-06-16 00:51:29 -0700

    added year selector using leaflet ctrl

--- alex/static/js/logic.js
+++ alex/static/js/logic.js
@@ -1,3 +1,61 @@
+// Range of data, from 2004 to 2020 (not counting 2020)
+var timeline = [2004,2020];
+
+// Load in geojson data
+var geoURL = "https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson";
+var fireURL = "data/brazil_states_deforestation.csv";
+// Container for all the different layers we create.
+// Each year gets its own layer so we can easily switch between them
+var firesLayer = {};
+
+// Create the base map
+var mapBoxMap = GenerateMap(); 
+// Refresh the map using the geojson data and the deforestation data.
+RefreshMap();
+
+// Grabs the geojson data for each state and grabs the csv data for forest fires
+// Parses and converts to javascript objects
+// Adds the fires data for each state in the loaded geojson data
+// Will Create features using updated geojson
+function RefreshMap() 
+{
+  var states = ["AC","AM","AP","MA","MT","PA","RO","RR","TO"];
+
+  // Grab data with d3
+  d3.json(geoURL, function(geoData) {
+    d3.csv(fireURL, function(fireData) {
+
+      fireData.forEach(function(d) {
+        d.Year = d.Year;
+        for(var i = 0 ; i < states.length; i++)
+        {
+          d[states[i]] = + d[states[i]];
+        }
+        d["AMZ LEGAL"] = +d["AMZ LEGAL"];
+      });
+
+      for(var i = 0 ; i < geoData.features.length; i++)
+      {
+        var prop = geoData.features[i].properties;
+        if(states.indexOf(prop.sigla) != -1)
+        {
+          geoData.features[i].properties.fires = {};
+          for(var j = 0 ; j < fireData.length ; j++)
+          {
+            geoData.features[i].properties.fires[fireData[j]["Year"]] = fireData[j][prop.sigla]; 
+          }
+        }
+
+      }
+      console.log(geoData);
+      console.log(fireData);
+      CreateFeatures(geoData, fireData, mapBoxMap);
+    });
+  }); 
+};
+
+// Create the base map 
+// Creates the legen as well with the right colors and ranges
 function GenerateMap()
 { 
   // Creating our initial map object
@@ -40,110 +98,88 @@ function GenerateMap()
   return mapboxMap;
 } 
 
-var currentYear = "2004";
-// Load in geojson data
-var geoURL = "https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson";
-var fireURL = "data/brazil_states_deforestation.csv";
-
-// // <div id="slider"></div>
-// var sliderHtml = document.getElementById('slider');
-
-// noUiSlider.create(sliderHtml, {
-//     start: 2004,
-//     step: 1,
-//     range: {
-//         'min': 2004,
-//         'max': 2019
-//     },    
-// });
-// sliderHtml.noUiSlider.set(2004);
-
-// // When the slider value changes, update the input and span
-// sliderHtml.noUiSlider.on('set', function () {
-//   var yer = toString(sliderHtml.noUiSlider.get());
-//   console.log(yer);
-// });
-
-var mapBoxMap = GenerateMap(); 
-RefreshMap();
-
-function RefreshMap() 
-{
-  var states = ["AC","AM","AP","MA","MT","PA","RO","RR","TO"];
-
-  // Grab data with d3
-  d3.json(geoURL, function(geoData) {
-    d3.csv(fireURL, function(fireData) {
-
-      fireData.forEach(function(d) {
-        d.Year = d.Year;
-        for(var i = 0 ; i < states.length; i++)
-        {
-          d[states[i]] = + d[states[i]];
-        }
-        d["AMZ LEGAL"] = +d["AMZ LEGAL"];
-      });
-
-      for(var i = 0 ; i < geoData.features.length; i++)
-      {
-        var prop = geoData.features[i].properties;
-        if(states.indexOf(prop.sigla) != -1)
-        {
-          geoData.features[i].properties.fires = {};
-          for(var j = 0 ; j < fireData.length ; j++)
-          {
-            geoData.features[i].properties.fires[fireData[j]["Year"]] = fireData[j][prop.sigla]; 
-          }
-        }
-
-      }
-      console.log(geoData);
-      console.log(fireData);
-      CreateFeatures(geoData, fireData, mapBoxMap);
-    });
-  }); 
-};
-var firesLayer = null;
-
+// Creates all the layers on the map combining the geodata and fire data
+// Each different year in the fire data is used as a different layer
+// The geojson contains the data required to draw the different states with their borders
+// it is publicly available on github.
 function CreateFeatures(geoData, fireData, map) {
-  if (firesLayer != null) {
-    map.removeLayer(firesLayer);
+  // This is used by onEachFeature to use the correct fire data for each popup
+  var currentYear = 2004;
+
+  // delete all the layers if they were already added
+  if (firesLayer != null) 
+  {
+    for (var entry in firesLayer)
+    {
+      map.removeLayer(firesLayer[entry]);
+    }
+    firesLayer = {};
   };
 
-  // Define a function we want to run once for each feature in the features array
-  function onEachFeature(feature, layer) {
+  // For each different feature, in our case the amount of fires for the current year
+  // Add a new popup
+  function onEachFeature(feature, layer) 
+  {
+    // Add State name
     var popupValue = "<h3>" + feature.properties.name + "</h3>";
     if(feature.properties.fires != null)
     {
-      popupValue += "<hr><p>" + feature.properties.fires[currentYear] + "</p>";
+      // Add the fires for the current year
+      popupValue += "<hr><p> Fires recorded: " + feature.properties.fires[currentYear] + "</p>";
     }
 
-    layer.bindPopup(popupValue);    
+    // Add popup to current layer
+    layer.bindPopup(popupValue, {'className' : 'firePopup'});    
   }
 
-  firesLayer = L.geoJSON(geoData, {
-    onEachFeature: onEachFeature,
-    style: function(feature) {
-      if(feature.properties.fires != null)
+  // for each year in our timeline starting from 2004
+  for(var i = 0 ; i < timeline[1] - timeline[0]; i++)
+  {
+    // Get current year starting from our first.
+    var currYear = timeline[0] + i;
+    // update currentl year for onEachFeature
+    currentYear = currYear;
+
+    // Get geoJson from geodata which already contains the fires 
+    var currLayer = L.geoJSON(geoData, 
+    {
+      onEachFeature: onEachFeature,
+      // Color each state based on amount of fires for that year
+      style: function(feature) 
       {
-        var d = feature.properties.fires[currentYear];
-        return  GetColor(d);
+        if(feature.properties.fires != null)
+        {
+          var d = feature.properties.fires[currYear];
+          return  GetColor(d);
+        }
+        return {color: '#FFFFF', opacity:0.0};
       }
-      return {color: '#FFFFF', opacity:0.0};
-    }
-  });
-  map.addLayer(firesLayer);
+    });
+
+    // map.addLayer(currLayer);
+    firesLayer[currYear] = currLayer;
+  }
+  console.log(firesLayer);
+	L.control.layers(firesLayer).addTo(map);
 }
 
+// Returns the color for a specified value
+// the colors & range is currently set in the function itself
 function GetColor(value) {
-  var colors = ["green", "darkgreen", "blue","yellow", "orange", "red"]
+  // Map of colors to the different range of values
+  var colors = ["#D9DBF1", "#85C7F2", "#175676","#FFE733", "#F57200", "#B80000"]
   var ranges = [500,1000,2000,4000,8000,12000]
   for(var i = 0 ; i < ranges.length; i++)
   {
+    // if value is less than current range value
+    // assign the color at the same index
     if(value < ranges[i])
     {
       return {color:colors[i],opacity:0.7};
     }
   }
-  return {color: 'red', opacity:0.7};
+
+  // if value is bigger than all our ranges
+  // just return color value
+  return {color: colors[colors.length - 1], opacity:0.7};
 };
